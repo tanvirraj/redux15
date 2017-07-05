5e5260699c419ed9bd6416c8b68d2d5b889243f2
tanvir raj <rajbp02@gamil.com>
2017-07-05 19:58:40 +0600

    Recreate new app

--- crud/crudx/src/App.js
+++ crud/crudx/src/App.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {Route,Link} from 'react-router-dom';
-import GamesPage from './GamesPage';
-
 
 class App extends Component {
   render() {
@@ -16,9 +13,6 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-
-        <Link to="/games">Games</Link>
-        <Route exact path="/games" component={GamesPage} />
       </div>
     );
   }
