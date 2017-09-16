357c8459fcf40c08c3c624cb5ae35d736e9c304b
tanvir raj <rajbp02@gamil.com>
2017-09-16 16:00:12 +0600

    Add basic layout

--- redux-crud/src/index.js
+++ redux-crud/src/index.js
@@ -4,5 +4,28 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore, applyMiddleware } from 'redux';
+
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+
+
+import rootReducer from './rootReducer';
+
+
+const store = createStore(
+    rootReducer,
+    composeWithDevTools(
+        applyMiddleware(thunk)
+    )
+);
+
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root'));
 registerServiceWorker();
---
+++ redux-crud/src/reducers/games.js
@@ -0,0 +1,13 @@
+const initialState = [
+    {'name': "Tanvir"},
+    {'name': "Sumit"},
+    {'name': "Masuq"}
+]
+
+
+export default function games(state=initialState, action={}){
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
